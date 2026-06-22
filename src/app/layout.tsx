34fa718a60cdfb6283a26b71814ccd7b2050da13
arronviet <quuocvit@gmail.com>
2026-06-22 20:26:09 +0700

    add saiyan edition background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,43 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
-      <body className={`${inter.variable} antialiased`}>
+      <body
+        className={`${inter.variable} antialiased bg-black text-white overflow-x-hidden`}
+      >
+        {/* Background */}
+        <div className="fixed inset-0 -z-10 overflow-hidden">
+
+          {/* Goku */}
+          <img
+            src="/goku-bg.jpg"
+            alt=""
+            className="
+              absolute right-0 bottom-0
+              h-full object-cover
+              opacity-[0.08]
+              blur-[1px]
+              pointer-events-none
+              select-none
+            "
+          />
+
+          {/* Ultra Instinct aura */}
+          <div
+            className="
+              absolute right-[-150px]
+              bottom-[-150px]
+              w-[900px]
+              h-[900px]
+              rounded-full
+              bg-cyan-400/10
+              blur-[220px]
+            "
+          />
+
+          {/* Dark overlay */}
+          <div className="absolute inset-0 bg-black/85" />
+        </div>
+
         <Providers>{children}</Providers>
       </body>
     </html>
